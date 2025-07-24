d42da22581c61dda422dfe9c8b1960226dbadba7
Andrew Goodrick-Werner <andrew.gw@me.com>
2025-07-24 14:42:26 -0400

    Initializes math functions of the calculator

---
+++ js/math.js
@@ -0,0 +1,54 @@
+//
+//  Calculator Math Functions
+//
+//  The math functions of the calculator
+//
+
+//
+//  Add
+//
+//  Adds operand
+//
+function add() {
+
+}
+
+
+//
+//  Subtract
+//
+//  Subtracts operand
+//
+function subtract() {
+
+}
+
+
+//
+//  Multiply
+//
+//  Multiplies operand
+//
+function multiply() {
+
+}
+
+
+//
+//  Divide
+//
+//  Divides operand
+//
+function divide() {
+
+}
+
+
+//
+//  Sum
+//
+//  Calculates Sum
+//
+function sum() {
+
+}
