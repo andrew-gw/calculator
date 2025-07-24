3d802c9f2149aff872442f76d02727e448a557d3
Andrew Goodrick-Werner <andrew.gw@me.com>
2025-07-24 14:41:56 -0400

    Sets up main functions of the calculator

---
+++ js/main.js
@@ -0,0 +1,52 @@
+//
+//  Output Digit
+//
+//  Outputs a digit
+//
+//  @param digit Number
+//
+function output(digit) {
+
+}
+
+
+//
+//  Append Point
+//
+//  Appends a point
+//
+//  @param digit Number
+//
+function point(digit) {
+
+}
+
+
+//
+//  Sign
+//
+//  Toggles sign +/-
+//
+function sign() {
+
+}
+
+
+//
+//  Percentage
+//
+//  Calculates percentage
+//
+function percentage() {
+
+}
+
+
+//
+//  Clear
+//
+//  Clears memory
+//
+function clear() {
+
+}
